f0500f3d948c1dd9f2e7e3cd5d1aa8a921fca218
Uther Arnt <utherharnt@gmail.com>
2026-05-25 23:06:08 -0300

    Implementação da funcionalidade Salvar com mensagens de sucesso e erro

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -23,12 +23,28 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
+    conn = new conectaDAO().connectDB();
+    String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+    
+    try {
+
+        prep = conn.prepareStatement(sql);
+        prep.setString(1, produto.getNome());
+        prep.setInt(2, produto.getValor());
+        prep.setString(3, produto.getStatus());
+        prep.execute();
         
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+    } catch (SQLException erro) {
+        System.out.println("Erro ao cadastrar produto no DAO: " + erro.getMessage());
+    } finally {
+        try {
+            if (prep != null) prep.close();
+            if (conn != null) conn.close();
+        } catch (SQLException e) {
+            System.out.println("Erro ao fechar conexões: " + e.getMessage());
+        }
     }
+}
     
     public ArrayList<ProdutosDTO> listarProdutos(){
         
